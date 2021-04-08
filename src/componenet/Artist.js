afb7c8a4a308a57b0253c08c15b687535f618bd2
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-04-08 10:56:01 +0300

    link to spotify

--- src/componenet/Artist.js
+++ src/componenet/Artist.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./css/artist.css";
 
 const Artist = ({ artistData }) => {
-	console.log(artistData.images[0].url);
 	return (
 		<div className="artistContainer">
-			<div
-				className="background"
-				style={{
-					backgroundImage: `url(${artistData.images[0].url})`,
-				}}
-			></div>
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href={artistData.external_urls.spotify}
+			>
+				<div
+					className="background"
+					style={{
+						backgroundImage: `url(${artistData.images[0].url})`,
+					}}
+				></div>
+			</a>
 			<h2>{artistData.name}</h2>
 			<p>followers {artistData.followers.total}</p>
 			<p>generes: {artistData.genres.join("-")}</p>
